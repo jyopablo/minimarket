ce1d0c9eee994ed5529e95aa1789868d598bd78a
Juan González <2986910430101@ingenieria.usac.edu.gt>
2021-07-07 23:49:21 -0600

    Casi se completo el la pagina de inicio

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+#_____________Pagina Index.html_______________
 @app.route('/')
 def index():
     return render_template('index.html')
+#_____________Pagina de Productos1______________
+
+
 
 if __name__=='__main__':
     app.run(threaded=True,debug=True,port=8000)
