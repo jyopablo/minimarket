ec0e323e0b7967dbfbced46f949da780c10b0b6c
yopablo <2986910430101@ingenieria.usac.edu.gt>
2021-07-18 12:23:25 -0600

    se pudo obtener el primer formulario de te frio

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template,request
+from werkzeug.utils import redirect
 
 carrito=[]
 
@@ -9,11 +10,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 #_____________Pagina de bebidas_frias______________
-@app.route('/bebidas_frias')
+@app.route('/bebidas_frias', methods=['GET','POST'])
 def bebidas_frias(): 
     frias=['Te frio','Cocacola','Pepsi','Cerveza']
     precios=['34.50','15.01','20.40','30.10']
-    return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios)
+    try:
+        if request.method=="POST":
+            producto_seleccionado=request.form.get("producto0")
+            precio_producto=request.form.get("precio0")
+            print(producto_seleccionado)
+            print(precio_producto)
+            return redirect(url_for('bebias_frias'))
+        else:
+            return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios)
+    except:
+        return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios)    
 
 
 
