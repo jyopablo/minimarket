108101afdd03d05d6c1edb6b98900d3d90d21211
yopablo <2986910430101@ingenieria.usac.edu.gt>
2021-07-18 16:34:13 -0600

    se agregaron listas y variable validar en app.py

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
 from flask import Flask
 from flask import render_template,request
 from werkzeug.utils import redirect
-
+validar=False
 carrito=[]
+precio_carrito=[]
+
 
 app = Flask(__name__)
 #_____________Pagina Index.html_______________
 @app.route('/')
 def index():
-    return render_template('index.html')
+    global validar
+    return render_template('index.html',validar=validar)
 #_____________Pagina de bebidas_frias______________
 @app.route('/bebidas_frias', methods=['GET','POST'])
-def bebidas_frias(): 
+def bebidas_frias():
+    global validar 
     frias=['Te frio','Cocacola','Pepsi','Cerveza']
     precios=['34.50','15.01','20.40','30.10']
     try:
         if request.method=="POST":
-            producto_seleccionado=request.form.get("producto0")
-            precio_producto=request.form.get("precio0")
-            print(producto_seleccionado)
-            print(precio_producto)
+            for i in range(0,4):
+                producto_seleccionado=request.form.get("producto"+str(i))
+                precio_producto=request.form.get("precio"+str(i))
+                if producto_seleccionado != None and precio_producto != None:
+                    break
+            carrito.append(producto_seleccionado)
+            precio_carrito.append(precio_producto)
+            validar=True
             return redirect(url_for('bebias_frias'))
         else:
-            return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios)
+            return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios,validar=validar)
     except:
-        return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios)    
+        return render_template('bebidas_frias.html', lista_bebidas_frias=frias,lista_precios=precios,validar=validar)    
 
 
 
