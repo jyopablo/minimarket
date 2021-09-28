32f70ef51e4b103a271357dd751fb9dddc10ae33
yopablo017 <2986910430101@ingenieria.usac.edu.gt>
2021-09-28 00:28:37 -0600

    Se agrego la pagina y productos de botanas

--- app.py
+++ app.py
@@ -44,12 +44,12 @@ def bebidas_bebidas():
 @app.route('/abarrotes', methods=['GET','POST'])
 def abarrotes():
     global validar 
-    abarrotes=['Incaparina','Pasta','Ablandador y Sasonador','Arroz','Harrina de Panqueques','Avena','Mayonesa','Alimento para Perro','Avena','8 Cereales','Azucar','Compota','Lentejas','Oregano','Miel','Huevos','Sal','Consome','Aceite Commestible','Manteca Vegetal','Salsa','Vinagre','Te','Frambuesas']
-    precios=['8.20','7.50','7.00','6.05','10.50','12.00','9.40','300.00','12.50','12.50','6.00','3.50','11.95','7.95','15.00','28.00','7.00','12.75','14.00','12.00','10.50','15.00','13.00','15.00']
-    imgs=['1.jpg','10.jpg','13.jpg','21.jpg','22.jpg','26.jpg','27.jpg','32.jpg','34.jpg','36.jpg','41.jpg','43.jpg','49.jpg','50.jpg','51.jpg','59.jpg','73.jpg','74.jpg','82.jpg','86.jpg','91.jpg','94.jpg','96.jpg','105.jpg']
+    abarrotes=['Incaparina','Pasta','Ablandador y Sasonador','Arroz','Harrina de Panqueques','Avena','Mayonesa','Alimento para Perro','Avena','8 Cereales','Azucar','Compota','Lentejas','Oregano','Miel','Huevos','Sal','Consome','Aceite Commestible','Manteca Vegetal','Salsa','Vinagre','Te','Frambuesas','Cereal','Cafe Instantaneo','Salsa','Frijoles Volteados','Canela en Raja','Frijol','flan','Pasta','Cafe Instantaneo','Sopa Instantanea','Sopa Pollo y Fideos']
+    precios=['8.20','7.50','7.00','6.05','10.50','12.00','9.40','300.00','12.50','12.50','6.00','3.50','11.95','7.95','15.00','28.00','7.00','12.75','14.00','12.00','10.50','15.00','13.00','15.00','18.00','21.00','15','7.00','7.00','9.25','6.00','7.50','30.50','4.70','3.70']
+    imgs=['1.jpg','10.jpg','13.jpg','21.jpg','22.jpg','26.jpg','27.jpg','32.jpg','34.jpg','36.jpg','41.jpg','43.jpg','49.jpg','50.jpg','51.jpg','59.jpg','73.jpg','74.jpg','82.jpg','86.jpg','91.jpg','94.jpg','96.jpg','105.jpg','106.jpg','107.jpg','110.jpg','115.jpg','123.jpg','126.jpg','150.jpg','161.jpg','163.jpg','174.jpg','189.jpg',]
     try:
         if request.method=='POST':
-            for i in range(0,24):
+            for i in range(0,35):
                 producto_seleccionado=request.form.get("producto"+str(i))
                 precio_producto=request.form.get("precio"+str(i))
                 if producto_seleccionado != None and precio_producto != None:
@@ -68,6 +68,33 @@ def abarrotes():
     except:
         return render_template('abarrotes.html', lista_imgs=imgs, lista_abarrotes=abarrotes,lista_precios=precios,validar=validar)    
 
+#_____________Pagina de botanas_______________
+@app.route('/botanas', methods=['GET','POST'])
+def botanas():
+    global validar 
+    botanas=['Papa','Palomitas','Frituras de Maiz','Cacahuates','Botanas Saldas','Barras Nutriticas','Nueces y Semillas']
+    precios=['8.00','2.00','5.00','20.95','7.20','16.90','16.00']
+    imgs=['90.jpg','167.jpg','17.jpg','120.jpg','84.jpg','53.jpg','146.jpg']
+    try:
+        if request.method=='POST':
+            for i in range(0,7):
+                producto_seleccionado=request.form.get("producto"+str(i))
+                precio_producto=request.form.get("precio"+str(i))
+                if producto_seleccionado != None and precio_producto != None:
+                    break
+            se_agrego_antes= producto_seleccionado in carrito
+            if se_agrego_antes == False:
+                carrito.append(producto_seleccionado)
+                precio_carrito.append(precio_producto)
+                validar=True
+            else:
+                return render_template('botanas.html', lista_imgs=imgs, lista_botanas=botanas,lista_precios=precios,validar=validar,se_agrego_antes=se_agrego_antes)
+            print(carrito)    
+            return redirect(url_for('botanas'))
+        else:
+            return render_template('botanas.html', lista_imgs=imgs, lista_botanas=botanas,lista_precios=precios,validar=validar)
+    except:
+        return render_template('botanas.html', lista_imgs=imgs, lista_botanas=botanas,lista_precios=precios,validar=validar) 
 
 
 if __name__=='__main__':
